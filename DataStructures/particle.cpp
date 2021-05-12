ea9e0b01f01952146036706a802dcc0bbf4ad9ad
dsaenzl <dsaenzl@unal.edu.co>
2021-05-12 08:56:55 -0500

    Desarrollo

--- DataStructures/particle.cpp
+++ DataStructures/particle.cpp
@@ -3,22 +3,26 @@
 
 const double G=9.81;
 const double dt =0.01;
+const double K=101.9;
 struct particle{
     float mass=0.0;
     double y=0.0;
     double vy=0.0;
     double fy=0.0;
+    double rad=0.0;
+    double delta=0.0;
 };
 void fuer(std::vector<particle> &a);
 void posv(std::vector<particle> &a);
 int main(void){
     std::vector<particle> point;
-    point.reserve(10);
     point.resize(1);
-    point[0].y=20;
+    //Inicializar
+    point[0].y=2.36;
     point[0].vy=0.0;
-    point[0].mass=10.0;
-    for(int ii=0;ii<100;ii++){
+    point[0].mass=1.98;
+    point[0].rad=0.17;
+    for(int ii=0;ii<500;ii++){
         fuer(point);
         posv(point);
         std::cout << ii*dt << "\t" 
@@ -29,9 +33,19 @@ int main(void){
 void fuer(std::vector<particle> &a){
     for(auto &x : a){
         x.fy=0.0;
+        x.delta=x.rad-x.y;
     }
-    for(auto &x : a){
-        x.fy-=x.mass*G;
+    for(int ii=0;ii<a.size();ii++){
+        if(a[ii].delta<0){
+            for(auto &x : a){
+                x.fy-=x.mass*G;
+            }
+        }
+        else{
+            for(auto &x : a){
+                x.fy+=a[ii].delta*K;
+            }
+        }
     }
 }
 void posv(std::vector<particle> &a){
